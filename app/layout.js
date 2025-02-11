4a35b5f54098788aae4fe0f89b016ee667ce92e4
JavascriptExpert2020 <muradshahnawaz1@gmail.com>
2025-02-11 23:02:00 +0500

    minor changes

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Sameem Abbas - Software Developer",
+  title: "Portfolio of Sameem Abbas | Software Developer",
   description:
     "This is the portfolio of Sameem Abbas. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
